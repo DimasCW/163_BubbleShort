12d1e9d8851b419c5674ccc033d61bcb3ca975a4
DimasCW <cdimas292@gmail.com>
2023-03-17 11:29:09 +0700

    menembahkan fungsi/prosedur void display(){ }

--- BubbleShort.cpp
+++ BubbleShort.cpp
@@ -4,14 +4,42 @@
 #include <iostream>
 using namespace std;
 
-int a[20];
+int a[20];a
 int n;
 
 void input() {
     while (true) {
         cout << "Masukkan banyaknhjya elemen pada array : ";
-        cin>>
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
     }
+    cout<<endl;
+    cout << "=========================="<<endl;
+    cout << "Masukkan Elemen Array"<< endl;
+    cout << "=========================="<<endl;
+
+    for (int i = 1; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ":";
+        cin >> a[i];
+    }
+
+}
+
+
+void display() {
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "==========================" << endl;
+
+    for (int j = 0; j < n; j++) {
+        cout << a[j]<<endl;
+    }
+    cout << endl;
 }
 
 
